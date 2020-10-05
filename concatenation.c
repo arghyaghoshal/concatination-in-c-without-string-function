59a4822ebb37278818d89bee564fe8e39ebde21d
arghyaghoshal <72149112+arghyaghoshal@users.noreply.github.com>
2020-10-05 12:54:44 +0530

    Add files via upload

---
+++ concatenation.c
@@ -0,0 +1,36 @@
+// C Program to concatenate 
+// two strings without using strcat 
+
+#include <stdio.h> 
+
+int main() 
+{ 
+    
+	char str1[100],str2[100],str3[100]; 
+	int i = 0, j = 0; 
+
+	
+	printf("\nEnter the First string:-\n");
+    scanf("%s",&str1); 
+	printf("\nEnter second string:-\n"); 
+    scanf("\n%s",&str2); 
+ 
+	while (str1[i] != '\0') { 
+		str3[j] = str1[i]; 
+		i++; 
+		j++; 
+	} 
+ 
+	i = 0; 
+	while (str2[i] != '\0') { 
+		str3[j] = str2[i]; 
+		i++; 
+		j++; 
+	} 
+	str3[j] = '\0'; 
+ 
+	printf("\nConcatenated string: %s", str3); 
+
+	getch();
+	return 0; 
+} 
